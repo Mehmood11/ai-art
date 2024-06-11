27e85f29733850f8ee136c9c44d4847eb061db4a
Mehmood khan <mehmood.khan@ceative.co.uk>
2024-06-11 10:58:17 +0500

    add: Scroll to top button added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Headers from "./components/Headers";
 import HeroSection from "./components/HeroSection";
 import Categories from "./components/Category";
 import ArtList from "./components/ArtList";
-import InfiniteScroll from "./test";
 
 const categories = [
   "mobile",
@@ -18,6 +17,29 @@ const categories = [
 
 function App() {
   const [activeCategory, setActiveCategory] = useState("mobile");
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
 
   return (
     <div className="main-app">
@@ -30,7 +52,16 @@ function App() {
         setActiveCategory={setActiveCategory}
       />
       <ArtList activeCategory={activeCategory} />
-      {/* <InfiniteScroll /> */}
+      <div className="scroll-up-button">
+        {isVisible && (
+          <button onClick={scrollToTop} className="scroll-button">
+            <img
+              src="https://cognise.art/static/images/back_to_top.svg"
+              alt="scroll-button"
+            />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
